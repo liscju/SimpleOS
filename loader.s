64ecb54e0a3b5a8e164736591314e85506f07200
liscju <piotr.listkiewicz@gmail.com>
2015-09-24 23:33:31 +0200

    Introduced makefile to automate building/running os in bochs emulator,removed useless elf builds

--- loader.s
+++ loader.s
@@ -1,9 +1,17 @@
 global loader                  ; the entry symbol for ELF
 
+extern kmain                   ; kmain is defined in kmain.c
+
 MAGIC_NUMBER equ 0x1BADB002    ; define the magic number constant
 FLAGS        equ 0x0           ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER ; calculate the checksum
                                ; (magic number + checksum + flags should eq 0) 
+KERNEL_STACK_SIZE equ 4096     ; size of stack in bytes
+
+section .bss
+align 4
+kernel_stack:
+	resb KERNEL_STACK_SIZE
 
 section .text:                 ; start of the text (code) section
 align 4                        ; the code must be 4 byte aligned
@@ -15,5 +23,10 @@ align 4                        ; the code must be 4 byte aligned
 
 loader:                        ; the loader label(defined as entry point in linker script)
 	mov eax, 0xCAFEBABE    ; place the number in the registry
+	mov esp, kernel_stack+KERNEL_STACK_SIZE ; set up the stack
+
+enter_kmain:
+	call kmain	       ; call C function to finally turn into C
+
 .loop:
 	jmp .loop              ; loop forever
