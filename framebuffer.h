4226f7d50c8be8b01136dd73b6c090dced025c2f
liscju <piotr.listkiewicz@gmail.com>
2015-09-30 11:49:37 +0200

    Added simple framebuffer implementation to write chars to screen

---
+++ framebuffer.h
@@ -0,0 +1,4 @@
+#define FB_GREEN 2
+#define FB_DARK_GREY 8
+
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
--- kmain.c
+++ kmain.c
@@ -1,3 +1,5 @@
+#include "framebuffer.h"
 
 void kmain() {
+	fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
 }
